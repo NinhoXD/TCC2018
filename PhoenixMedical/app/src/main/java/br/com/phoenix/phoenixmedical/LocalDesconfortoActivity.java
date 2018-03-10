2815814d7b9068ba001afe62aa65a8ac1e5da1fb
Diego de Melo Gonzaga <diego-gonzaga@live.com>
2018-03-10 08:33:37 -0300

    Adicionado o aplicativo em desenvolvimento Android

---
+++ PhoenixMedical/app/src/main/java/br/com/phoenix/phoenixmedical/LocalDesconfortoActivity.java
@@ -0,0 +1,13 @@
+package br.com.phoenix.phoenixmedical;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LocalDesconfortoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_local_desconforto);
+    }
+}
