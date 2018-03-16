922f014583f0297652f81b45fc7c806f600c381a
Diego de Melo Gonzaga <diego-gonzaga@live.com>
2018-03-15 22:24:35 -0300

    Adicionado activity de Exames.

---
+++ PhoenixMedical/app/src/main/java/br/com/phoenix/phoenixmedical/ExameActivity.java
@@ -0,0 +1,15 @@
+package br.com.phoenix.phoenixmedical;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by diego-gonzaga on 14/03/18.
+ */
+
+public class ExameActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_questoes_desconforto);
+    }
+}
